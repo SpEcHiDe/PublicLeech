55591c92946adbca7026af486f6ecba5ff895747
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-04-24 02:29:54 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -127,58 +127,24 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
                 "starting upload of {}".format(os.path.basename(local_file_name))
             )
         if local_file_name.upper().endswith(("MKV", "MP4", "WEBM")):
-            metadata = extractMetadata(createParser(local_file_name))
-            duration = 0
-            if metadata.has("duration"):
-                duration = metadata.get('duration').seconds
-            #
-            width = 0
-            height = 0
             thumb_image_path = None
-            if os.path.exists(thumbnail_location):
+            if os.path.isfile(thumbnail_location):
                 thumb_image_path = await copy_file(
                     thumbnail_location,
                     os.path.dirname(os.path.abspath(local_file_name))
                 )
-            else:
-                thumb_image_path = await take_screen_shot(
-                    local_file_name,
-                    os.path.dirname(os.path.abspath(local_file_name)),
-                    (duration / 2)
-                )
-                # get the correct width, height, and duration for videos greater than 10MB
-                if os.path.exists(thumb_image_path):
-                    metadata = extractMetadata(createParser(thumb_image_path))
-                    if metadata.has("width"):
-                        width = metadata.get("width")
-                    if metadata.has("height"):
-                        height = metadata.get("height")
-                    # resize image
-                    # ref: https://t.me/PyrogramChat/44663
-                    # https://stackoverflow.com/a/21669827/4723940
-                    Image.open(thumb_image_path).convert(
-                        "RGB"
-                    ).save(thumb_image_path)
-                    img = Image.open(thumb_image_path)
-                    # https://stackoverflow.com/a/37631799/4723940
-                    img.resize((320, height))
-                    img.save(thumb_image_path, "JPEG")
-                    # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-            #
+            # if a file, don't upload "thumb"
             thumb = None
             if thumb_image_path is not None and os.path.isfile(thumb_image_path):
                 thumb = thumb_image_path
-            # send video
-            sent_message = await message.reply_video(
-                video=local_file_name,
+            #
+            # send document
+            sent_message = await message.reply_document(
+                document=local_file_name,
                 # quote=True,
+                thumb=thumb,
                 caption=caption_str,
                 parse_mode="html",
-                duration=duration,
-                width=width,
-                height=height,
-                thumb=thumb,
-                supports_streaming=False,
                 disable_notification=True,
                 reply_to_message_id=message.reply_to_message.message_id,
                 progress=progress_for_pyrogram,

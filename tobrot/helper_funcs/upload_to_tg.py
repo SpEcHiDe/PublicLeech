8e3052f23404c4a9a4e9f06e077c72312a00964d
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-04-24 05:51:32 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -149,7 +149,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
                 reply_to_message_id=message.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                    "trying to upload",
+                    "trying to upload {}".format(os.path.basename(local_file_name)),
                     message_for_progress_display,
                     start_time
                 )

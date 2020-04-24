07302a987ca56444d2bda2eb652f5c6f65defb14
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-04-24 12:48:52 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -39,32 +39,34 @@ async def incoming_message_f(client, message):
     LOGGER.info(dl_url)
     LOGGER.info(cf_name)
     if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
-        # start the aria2c daemon
-        aria_i_p = await aria_start()
-        LOGGER.info(aria_i_p)
-        current_user_id = message.from_user.id
-        # create an unique directory
-        new_download_location = os.path.join(
-            DOWNLOAD_LOCATION,
-            str(current_user_id),
-            str(time.time())
-        )
-        # create download directory, if not exist
-        if not os.path.isdir(new_download_location):
-            os.makedirs(new_download_location)
-        await i_m_sefg.edit_text("trying to download")
-        # try to download the "link"
-        sagtus, err_message = await call_apropriate_function(
-            aria_i_p,
-            dl_url,
-            new_download_location,
-            i_m_sefg,
-            is_zip
-        )
-        if not sagtus:
-            # if FAILED, display the error message
-            await i_m_sefg.edit_text(err_message)
+        akcm = dl_url.split('\n')
+        for akc_url in akcm:
+            await i_m_sefg.edit_text("extracting links")
+            # start the aria2c daemon
+            aria_i_p = await aria_start()
+            LOGGER.info(aria_i_p)
+            current_user_id = message.from_user.id
+            # create an unique directory
+            new_download_location = os.path.join(
+                DOWNLOAD_LOCATION,
+                str(current_user_id),
+                str(time.time())
+            )
+            # create download directory, if not exist
+            if not os.path.isdir(new_download_location):
+                os.makedirs(new_download_location)
+            await i_m_sefg.edit_text("trying to download")
+            # try to download the "link"
+            sagtus, err_message = await call_apropriate_function(
+                aria_i_p,
+                akc_url,
+                new_download_location,
+                i_m_sefg,
+                is_zip
+            )
+            if not sagtus:
+                # if FAILED, display the error message
+                await i_m_sefg.edit_text(err_message)
     else:
         await i_m_sefg.edit_text("**ERR**! What have you entered. Please read /help")
 

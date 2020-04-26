87acd5010633ff1b61e736a63133e99321acb21e
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-04-26 10:34:25 +0530

    Revert "Update download_aria_p_n.py"
    This reverts commit 434ae09fdfe2d58c1d56ec133315e5192c57a677.

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -43,7 +43,7 @@ async def aria_start():
     aria2_daemon_start_cmd.append("--rpc-listen-all=false")
     aria2_daemon_start_cmd.append(f"--rpc-listen-port={ARIA_TWO_STARTED_PORT}")
     aria2_daemon_start_cmd.append("--rpc-max-request-size=1024M")
-    aria2_daemon_start_cmd.append("--enable-peer-exchange=true")
+    aria2_daemon_start_cmd.append("--enable-peer-exchange=True")
     aria2_daemon_start_cmd.append("--seed-ratio=1.0")
     aria2_daemon_start_cmd.append("--seed-time=0")
     aria2_daemon_start_cmd.append("--split=10")

87de4e1495675809e5fc2a4c490f5ef35fe95503
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-04-26 12:37:12 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -25,7 +25,7 @@ from tobrot import (
 from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
 
 from tobrot.plugins.new_join_fn import new_join_f, help_message_f, rename_message_f
-from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f
+from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_split_message_f
 from tobrot.plugins.status_message_fn import (
     status_message_f,
     cancel_message_f,

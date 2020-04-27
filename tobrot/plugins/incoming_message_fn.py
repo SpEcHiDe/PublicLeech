986b1705c496e37c28ef518ea37f3c294a485e15
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-04-28 04:33:07 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -19,6 +19,7 @@ from tobrot import (
 )
 
 
+import aria2p
 import time
 from tobrot.helper_funcs.extract_link_from_message import extract_link
 from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function, aria_start
@@ -26,6 +27,16 @@ from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
 
+async def incoming_purge_message_f(client, message):
+    """/purge command"""
+    i_m_sefg2 = await message.reply_text("Purging...", quote=True)
+    if await AdminCheck(client, message.chat.id, message.from_user.id):
+        aria_i_p = await aria_start()
+        # Show All Downloads
+        downloads = aria_i_p.get_downloads()
+		for download in downloads:
+			LOGGER.info(download.remove(force=True))
+    await i_m_sefg.delete()
 
 async def incoming_message_f(client, message):
     """/leech command"""

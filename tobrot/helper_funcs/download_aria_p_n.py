faed4e392205faa08900e491caffd24b9afde031
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-04-29 13:27:37 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -153,6 +153,8 @@ async def call_apropriate_function(
         # ref: https://github.com/out386/aria-telegram-mirror-bot/blob/master/src/download_tools/aria-tools.ts#L194
         # archive the contents
         check_if_file = await create_archive(to_upload_file)
+        LOGGER.info("check_if_file")
+        LOGGER.info(check_if_file)
         if check_if_file is not None:
             to_upload_file = check_if_file
     #

b872a1712de56c6e34153e5807cc2abf23793185
Shrimadhav U K <SpEcHIDe@users.noreply.github.com>
2020-06-27 21:40:12 +0530

    something is broken here. hmm

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -96,19 +96,10 @@ async def button(bot, update: CallbackQuery):
                 yt_dl_pass_word,
                 user_working_dir
             )
-            if thumb_image is not None:
-                await i_m_sefg.reply_to_message.reply_photo(
-                    photo=thumb_image,
-                    quote=True,
-                    caption=text_message,
-                    reply_markup=reply_markup
-                )
-                await i_m_sefg.delete()
-            else:
-                await i_m_sefg.edit_text(
-                    text=text_message,
-                    reply_markup=reply_markup
-                )
+            await i_m_sefg.edit_text(
+                text=text_message,
+                reply_markup=reply_markup
+            )
         else:
             await i_m_sefg.delete()
 

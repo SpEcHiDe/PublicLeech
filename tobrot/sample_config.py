4b1bf083499b21cc7d5bb3ba8969c23dc7a9e8d8
Shrimadhav U K <SpEcHIDe@users.noreply.github.com>
2020-06-27 19:43:18 +0530

    make AUTH_CHANNEL an array

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -7,8 +7,8 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
-    # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", -100))
+    # array to store the channel ID who are authorized to use the bot
+    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size

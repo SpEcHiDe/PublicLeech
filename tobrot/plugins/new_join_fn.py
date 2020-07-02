38c7a21032c26b28bad62157ab8558473ab15b58
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-02 12:18:00 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
+        f"",
         quote=True
     )
 

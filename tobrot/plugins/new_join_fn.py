048f8cfd6b2cd2afab6a092250feb349fccf27fe
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-02 12:25:44 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"",
+        f"Hi Welcome Read Pinned Meesage",
         quote=True
     )
 

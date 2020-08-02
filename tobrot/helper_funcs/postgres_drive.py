15ee7827daa13ff4e824742f0c306a94d65bc398
YashDK <yashkhadse@gmail.com>
2020-08-02 23:37:55 +0530

    little addition

--- tobrot/helper_funcs/postgres_drive.py
+++ tobrot/helper_funcs/postgres_drive.py
@@ -24,6 +24,8 @@ class DataBaseHandle:
         else:
             self._block = False
         
+        if self._block:
+            return
         self._conn = psycopg2.connect(self._dburl)
         
         download_table = """
@@ -134,4 +136,6 @@ class DataBaseHandle:
     def __del__(self):
         """Close connection so that the threshold is not exceeded
         """
+        if self._block:
+            return
         self._conn.close()
